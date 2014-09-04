4cff7b46d4435045b8588c0dbcef1276960d7341
TEMA <lineartem@gmail.com>
2014-09-04 15:25:52 +0300

    all images in the DOM enabled with red border

--- elogio@.firefox.extention.com/chrome/content/script.js
+++ elogio@.firefox.extention.com/chrome/content/script.js
@@ -2,7 +2,6 @@
  * Created by TEMA on 04.09.2014.
  */
 var TTElogioRunner = function() {
-    var curVersion = "1.0";
     var showAlert = function(messageID) {
         var alertsService = Components.classes["@mozilla.org/alerts-service;1"].getService(Components.interfaces.nsIAlertsService);
         var stringBundle = document.getElementById("elogio-string-bundle");
@@ -10,62 +9,24 @@ var TTElogioRunner = function() {
         var message = stringBundle.getString(messageID);
         alertsService.showAlertNotification('', title, message, false, '', null);
     };
-    var installButton = function(toolbarId, id, afterId) {
-        if (!document.getElementById(id)) {
-            var toolbar = document.getElementById(toolbarId);
-
-            // If no afterId is given, then append the item to the toolbar
-            var before = null;
-            if (afterId) {
-                let elem = document.getElementById(afterId);
-                if (elem && elem.parentNode == toolbar)
-                    before = elem.nextElementSibling;
-            }
-            toolbar.insertItem(id, before);
-            toolbar.setAttribute("currentset", toolbar.currentSet);
-            document.persist(toolbar.id, "currentset");
-
-            if (toolbarId == "addon-bar") {
-                toolbar.collapsed = false;
-            }
-        }
-    };
 
     return {
         run: function() {
            showAlert('error1');
-        },onPageLoad:function(aEvent){
+        },
+        onPageLoad:function(aEvent){
             var doc = aEvent.originalTarget;
             if (doc.nodeName == "#document") {
                 var images=doc.getElementsByTagName('img');
-                alert(images.length);
+                for(var i=0;i<images.length;i++){
+                    images[i].style.border = "5px solid red";
+                }
             }
         },
         onWindowLoad:function(){
             var appcontent = document.getElementById("appcontent");
             if (appcontent)
                 appcontent.addEventListener("load", TTElogioRunner.onPageLoad, true);
-        },
-        init: function(extensions) {
-            var firstrun = Services.prefs.getBoolPref("extensions.elogio@.firefox.extention.com.firstrun");
-            if (firstrun) {
-                Services.prefs.setBoolPref("extensions.elogio@.firefox.extention.com.firstrun", false);
-                Services.prefs.setCharPref("extensions.elogio@.firefox.extention.com.installedVersion", curVersion);
-                /* Code related to firstrun */
-                installButton("nav-bar", "elogio-toolbar-button");
-                // The "addon-bar" is available since Firefox 4
-                installButton("addon-bar", "elogio-toolbar-button");
-            } else {
-                try {
-                    var installedVersion = Services.prefs.getCharPref("extensions.elogio@.firefox.extention.com.installedVersion");
-                    if (curVersion > installedVersion) {
-                        Services.prefs.setCharPref("extensions.elogio@.firefox.extention.com.installedVersion", curVersion);
-                        /* Code related to upgrade */
-                    }
-                } catch (ex) {
-                    /* Code related to a reinstall */
-                }
-            }
         }
     };
 }();
--- elogio@.firefox.extention.com/defaults/preferences/pref.js
+++
@@ -1,2 +0,0 @@
-pref("extensions.elogio@.firefox.extention.com.firstrun", true);
-pref("extensions.elogio@.firefox.extention.com.installedVersion", '0.0');
