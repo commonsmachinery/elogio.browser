4f9867348d5d3a8197ef8e30c50eaf74f24f827f
TEMA <lineartem@gmail.com>
2014-09-09 23:05:59 +0300

    script was optimized (search all elements which includes image contains in one function) don't need check repeating elements because we add elements to array only if it doesn't exist there. added hashMap which contains key=url of image and value=GUID (in the attribute)

--- elogio-firefox/data/content-script.js
+++ elogio-firefox/data/content-script.js
@@ -1,116 +1,134 @@
 (function () {
     'use strict';
     var loc = document.location.toString().substring(0, document.location.toString().lastIndexOf('/') + 1);
-    self.port.on("getElements", function (limitPixels) {
-        var count = 0;
-        //canonization of image url
-        function canonizeUrl(url, urlLocation) {
-            if (url) {
-                if (url.indexOf('/') === 0) {//if image into deep folder
-                    return urlLocation + url.substring(1, url.length);
-                }
-                if (url.indexOf('../') === 0) {//if image into upper folder
-                    urlLocation = urlLocation.substring(0, urlLocation.lastIndexOf('/'));
-                    return canonizeUrl(url.substring(3, url.length), urlLocation.substring(0, urlLocation.lastIndexOf('/') + 1));
-                }
+    var attributeOfElements = 'elogio';
+    var imagesHashMap = {};
+
+    function startsWith(st, prefix) {
+        if (st.indexOf(prefix) === 0) {
+            return true;
+        }
+        return false;
+    }
+
+    var guid = (function () {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+
+        return function () {
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                s4() + '-' + s4() + s4() + s4();
+        };
+    })();
+
+    function getElementByGUID(attribute, id, context) {
+        var nodeList = (context || document).getElementsByTagName('*');
+        for (var i = 0, n = nodeList.length; i < n; i++) {
+            var att = nodeList[i].getAttribute(attribute);
+            if (att && att === id) {
+                return nodeList[i];
             }
-            if (url.indexOf('data') === 0) {//if image is base64
+        }
+    }
+
+    function attachUrlAndGUID(inElem, url) {
+        var id = guid();
+        inElem.setAttribute(attributeOfElements, id);
+        imagesHashMap[url] = id;
+    }
+
+    document.deepCss = function (who, css) {
+        if (!who || !who.style) {
+            return '';
+        }
+        var elementStyle = css.replace(/\-([a-z])/g, function (a, b) {
+            return b.toUpperCase();
+        });
+        if (who.currentStyle) {
+            return who.style[elementStyle] || who.currentStyle[elementStyle] || '';
+        }
+        var elementDefaultView = document.defaultView || window;
+        if (who.style[elementStyle]) {
+            return who.style[elementStyle];
+        }
+        if (null !== elementDefaultView.getComputedStyle(who, "")) {
+            if (elementDefaultView.getComputedStyle(who, "").getPropertyValue(css)) {
+                return elementDefaultView.getComputedStyle(who, "").getPropertyValue(css);
+            }
+        }
+        return '';
+    };
+    function canonizeUrl(url, urlLocation) {
+        if (url) {
+            if (startsWith(url, 'http') || startsWith(url, 'www')) {
                 return url;
             }
-            return urlLocation + url;//if already canonized
+            if (startsWith(url, '/')) {//if image into deep folder
+                return urlLocation + url.substring(1, url.length);
+            }
+            if (startsWith(url, '../')) {//if image into upper folder
+                urlLocation = urlLocation.substring(0, urlLocation.lastIndexOf('/'));
+                return canonizeUrl(url.substring(3, url.length), urlLocation.substring(0, urlLocation.lastIndexOf('/') + 1));
+            }
+            if (startsWith(url, 'data')) {//if image is base64
+                return url;
+            }
+        } else {
+            return false;
         }
+        return urlLocation + url;//if already canonized
+    }
 
-        //traverse all items on the page
-        function arrayIndexOf(arr, what, index) {
-            index = index || 0;
-            var lengthOfInputArray = arr.length;
-            while (index < lengthOfInputArray) {
-                if (arr[index] === what) {
-                    return index;
-                }
-                ++index;
+    function arrayIndexOf(arr, what, index) {
+        index = index || 0;
+        var lengthOfInputArray = arr.length;
+        while (index < lengthOfInputArray) {
+            if (arr[index] === what) {
+                return index;
             }
-            return -1;
+            ++index;
         }
+        return -1;
+    }
 
-//traverse the css styles of the page
-        document.deepCss = function (who, css) {
-            if (!who || !who.style) {
-                return '';
-            }
-            var elementStyle = css.replace(/\-([a-z])/g, function (a, b) {
-                return b.toUpperCase();
-            });
-            if (who.currentStyle) {
-                return who.style[elementStyle] || who.currentStyle[elementStyle] || '';
-            }
-            var elementDefaultView = document.defaultView || window;
-            if (who.style[elementStyle]) {
-                return who.style[elementStyle];
-            }
-            if (null !== elementDefaultView.getComputedStyle(who, "")) {
-                if (elementDefaultView.getComputedStyle(who, "").getPropertyValue(css)) {
-                    return elementDefaultView.getComputedStyle(who, "").getPropertyValue(css);
-                }
-            }
-            return '';
-        };
-        function getAllBackgroundImages() {
-            var url, urlsImage = [], allDomElements = document.getElementsByTagName('*');
-            allDomElements = urlsImage.slice.call(allDomElements, 0, allDomElements.length);
-            while (allDomElements.length) {
-                url = document.deepCss(allDomElements.shift(), 'background-image');
+    function getAllImages() {
+        var url, current, urlsImage = [], allDomElements = document.getElementsByTagName('*');
+        allDomElements = urlsImage.slice.call(allDomElements, 0, allDomElements.length);
+        while (allDomElements.length) {
+            current = allDomElements.shift();
+            if (current instanceof HTMLImageElement) {//if img
+                url = canonizeUrl(current.src, loc);
+            } else {//if background
+                url = document.deepCss(current, 'background-image');
                 if (url) {
                     url = /url\(['"]?([^")]+)/.exec(url) || [];
                 }
-                url = url[1];
-                if (url && -1 === arrayIndexOf(urlsImage, url)) {
-                    urlsImage[urlsImage.length] = url;
-                }
+                url = canonizeUrl(url[1], loc);
             }
-            return urlsImage;
+            if (url && -1 === arrayIndexOf(urlsImage, url)) {
+                attachUrlAndGUID(current, url);
+                urlsImage[urlsImage.length] = url;
+            }
         }
+        return urlsImage;
+    }
 
-        function startsWith(st, prefix) {
-            if (st.indexOf(prefix) === 0) {
-                return true;
-            }
-            return false;
-        }
-
-        var imagesOnThePage = document.images;//all <img> on the page
-        var elementsToFiltering = getAllBackgroundImages();//all urls of images
-        var i;//for loop
-        for (i = 0; i < imagesOnThePage.length; i++) {
-            if (imagesOnThePage[i].src) {
-                elementsToFiltering.push(imagesOnThePage[i].src);
-            }
-        }
-        //canonization urls
-        for (i = 0; i < elementsToFiltering.length; i++) {
-            if (!startsWith(elementsToFiltering[i], 'http')) {
-                elementsToFiltering[i] = canonizeUrl(elementsToFiltering[i], loc);
-            }
-        }
-        //delete repeating urls
-        for (i = elementsToFiltering.length - 1; i > 0; i--) {
-            for (var j = i - 1; j >= 0; j--) {
-                if (elementsToFiltering[i] === elementsToFiltering[j]) {
-                    elementsToFiltering.splice(j, 1);
-                }
-            }
-        }
-
-        var imagesToOutPut = [];
-
+    self.port.on("getElements", function (limitPixels) {
+        var count = 0;
+        var elementsToFiltering = getAllImages();//all urls of images
+        imagesHashMap = {};
         function ifReadyThenSend() {//if all images loaded then we need to send it to Main.js
             if (count === elementsToFiltering.length - 1) {
                 self.port.emit("gotElement", imagesToOutPut);
             }
         }
 
+        var imagesToOutPut = [];
         //we need to filter all images by width*height>limit
-        function filterImages(inputImgs) {
+        function filterImages(inputImages) {
             var filteringImages = [];
             //if img loaded then count++ and save it if width*height of image >limit
             var onLoadImg = function () {
@@ -129,14 +147,14 @@
                 ifReadyThenSend();
                 return true;
             };
-            for (var i = 0; i < inputImgs.length; i++) {
+            for (var i = 0; i < inputImages.length; i++) {
                 filteringImages.push(new Image());
-                filteringImages[i].src = inputImgs[i];
+                filteringImages[i].src = inputImages[i];
                 filteringImages[i].addEventListener('load', onLoadImg);
                 filteringImages[i].addEventListener('error', imageOnError);
             }
         }
 
-        filterImages(elementsToFiltering);//run
+        filterImages(elementsToFiltering);//filter img and send it
     });
 })();
