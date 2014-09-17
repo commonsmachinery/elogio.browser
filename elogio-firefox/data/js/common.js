7336b8af9487fd4bfa5b89e1ab0b42868c4144bf
Dmitry Berezovsky <dmitri.berezovsky@logicify.com>
2014-09-17 21:37:00 +0300

    Added Observable and StateController

--- elogio-firefox/data/js/common.js
+++ elogio-firefox/data/js/common.js
@@ -7,9 +7,9 @@ function Elogio() {
     console.log('Elogio sandbox');
     // convert arguments to real array
     var args = Array.prototype.slice.call(arguments),
-        // callback should be the last element in the list of arguments
+    // callback should be the last element in the list of arguments
         callback = args.pop(),
-        // Module names can be passed as array or set of arguments
+    // Module names can be passed as array or set of arguments
         modules = (args[0] && typeof args[0] === 'string') ? args : (args[0] ? args[0] : []), i;
     // Explicitly call constructor if it is needed
     if (!(this instanceof Elogio)) {
@@ -29,7 +29,7 @@ function Elogio() {
 }
 
 Elogio.modules = Elogio.modules || {};
-Elogio.prototype.getModule = function(moduleName) {
+Elogio.prototype.getModule = function (moduleName) {
     "use strict";
     if (this.hasOwnProperty(moduleName)) {
         return this[moduleName];
@@ -38,6 +38,90 @@ Elogio.prototype.getModule = function(moduleName) {
         return null;
     }
 };
+
+Elogio.inherit = function (C, P) {
+    "use strict";
+    C.prototype = new P();
+    C.superClass = P.prototype;
+    C.prototype.constructor = C;
+};
+
+Elogio.Observable = function () {
+    "use strict";
+    var bus = {};
+
+    function validateEventName(eventName, self) {
+        if (self.events.indexOf(eventName) === -1) {
+            console.error('Event ' + eventName + 'is not supported by ' + self.constructor.name);
+            return false;
+        }
+        return true;
+    }
+
+    this.events = [];
+
+    this.emit = function (eventName, arg) {
+        var i, handlers;
+        if (!validateEventName(eventName, this)) {
+            return;
+        }
+        handlers = bus[eventName];
+        for (i = 0; i < handlers.length; i +=1) {
+            bus[i].apply(null, arg);
+        }
+    };
+
+    this.on = function (eventName, callback) {
+        var handlers;
+        if (!validateEventName(eventName, this)) {
+            return;
+        }
+        if (!bus[eventName]) {
+            bus[eventName] = [];
+        }
+        handlers = bus[eventName];
+        handlers[handlers.length] = callback;
+    };
+};
+
+/**
+ * Class allows to
+ * @constructor
+ */
+Elogio.StateController = function (initialState) {
+    "use strict";
+    var state = {};
+
+    this.isObservable = false;
+
+    this.get = function (propertyName, defaultValue) {
+        if (!(propertyName in state)) {
+            return defaultValue;
+        }
+        return state[propertyName];
+    };
+
+    this.set = function (propertyName, value) {
+        var oldValue = state[propertyName];
+        state[propertyName] = value;
+        if (this.isObservable) {
+            this.emit(this.constructor.events.onPropertyChanged, [propertyName, oldValue, value]);
+        }
+    };
+
+    if (initialState) {
+        state = initialState || {};
+        if (this.isObservable) {
+            this.emit(this.constructor.events.onInitialized);
+        }
+    }
+    this.events = this.constructor.events;
+};
+Elogio.StateController.events = {
+    onPropertyChanged:  'onChanged',
+    onInitialized:      'onInitialized'
+};
+Elogio.inherit(Elogio.StateController, Elogio.Observable);
 
 // If module is used in Chrome context
 if (typeof exports !== 'undefined') {
